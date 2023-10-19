9064e383d7037c6e246953fb634708b46d8e7ab2
Florian Isopp <florian.johannes.isopp@gmail.com>
2023-10-19 19:37:18 +0200

    play on test runner

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          but in the end this is just a dummy test.
+          lorem import
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import ReactDOM from 'react-dom';
 
-import { createRoot } from 'react-dom';
-import ReactTestUtils, { act } from 'react-dom/test-utils'; // ES6
-
+import { createRoot } from 'react-dom/client';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+var x = 1 + 1;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
